c7fcddc1885db27ea332cf8f4d3e3f4206b0a6c4
Quentin Lassalle <160498861+qlassalledecath@users.noreply.github.com>
2026-01-08 14:27:41 +0100

    Add comments to better explain EcommerceService methods

--- src/main/java/com/qlassalle/EcommerceService.java
+++ src/main/java/com/qlassalle/EcommerceService.java
@@ -3,10 +3,20 @@ package com.qlassalle;
 public class EcommerceService {
 
     public void sendProductToEcommerce(Product product) {
+        /*
+         TODO
+         We want to send a message to the ecommerce team via a message broker
+         We need to provide and implement this message broker dependency in a testable way
+         */
         System.out.printf("Sending product %d to ecommerce...", product.getId());
     }
 
     public void sendMessageToIncreaseStockToStockTeam(Product product) {
+        /*
+          TODO
+          We want to send a message to the stock team via a message broker
+          We need to provide and implement this message broker dependency in a testable way
+         */
         System.out.printf("Sending message to decrease stock for product %d...", product.getId());
     }
 }
